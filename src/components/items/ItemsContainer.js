42b3f5e4ce000792fdf95ab445dbbd1a85dca1f5
Rianna <riannaxcastro@gmail.com>
2022-01-22 18:09:36 -1000

    connected components

---
+++ src/components/items/ItemsContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ItemsContainer extends Component {
+    render() {
+        return (
+            <div>
+              <h1>All Items</h1>
+            </div>
+        )
+    }
+}
