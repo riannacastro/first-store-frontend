73a25c105b46e4edfa9572f0ae13f905b0bfff8f
Rianna <riannaxcastro@gmail.com>
2022-01-22 18:24:54 -1000

    created reducer function

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import store from './redux/store';
+import store from './redux/Store';
 
 ReactDOM.render(
   <React.StrictMode>
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -1,3 +1,6 @@
 import { applyMiddleware, createStore } from "redux";
+import thunk from 'redux-thunk';
+import reducer from "./reducers/Reducer";
+
 
 export default createStore(reducer, applyMiddleware(thunk))
