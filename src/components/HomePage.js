2840f2297fdb3247a0d97c7e123825b4a39c0ffa
Rianna <riannaxcastro@gmail.com>
2022-01-23 04:54:01 -1000

    css to homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -6,8 +6,8 @@ export default class HomePage extends Component {
     render() {
         return (
             <div className='header'>
-                <h1>welcome!! If you are a member, please sign in:</h1>
-
+                <h1>Welcome! If you are a member, please sign in:</h1>
+                < br />
                 <Link id="signin-btn" to="/signin">Sign In</Link>
             </div>
         )
