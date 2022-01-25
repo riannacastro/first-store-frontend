b7db7eff69bc7f3ad84c75b6dcea83a43492d9f6
Rianna <riannaxcastro@gmail.com>
2022-01-24 18:25:14 -1000

    add case to reducer

--- src/redux/reducers/Reducer.js
+++ src/redux/reducers/Reducer.js
@@ -1,4 +1,17 @@
-function reducer(state, action) {
+initialState = {
+    items: []
+}
 
+function reducer(initialState, action) {
+    switch(action.type) {
+
+        case "GET_ITEMS":
+
+        return {
+            ...initialState,
+            items: action.payload
+        };
+
+    }
 }
 export default reducer
