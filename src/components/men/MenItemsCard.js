cc18c1cc17d4d69172d45508a3563ac6152f0279
Rianna <riannaxcastro@gmail.com>
2022-01-27 14:18:35 -1000

    added css to men

--- src/components/men/MenItemsCard.js
+++ src/components/men/MenItemsCard.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 function MenItemsCard(props) {
     return (
-        <div>
-          {props.name} - {props.price}
+        <div className='container'>
+            <div className='card'>
+                <div className='contentBx'>
+                    {props.name} - {props.price}
+                    <div className='a'>
+                    <h3>Buy Now</h3>
+                    </div>
+                </div>
+            </div>
         </div>
       );
     }
