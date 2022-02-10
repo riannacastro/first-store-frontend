21077861706ef16b3e992f630ae343d105405bd6
Rianna <riannaxcastro@gmail.com>
2022-02-09 20:04:40 -1000

    create cart file

---
+++ src/components/cart/Cart.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+export default class Cart extends Component {
+
+    state = {
+        items: [
+            
+        ]
+    }
+
+  render() {
+    return <div></div>;
+  }
+}
